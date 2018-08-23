8384f35a426e6231438215ede09d9b13a10c3f84
Gu Junhua <jhgu@nao.cas.cn>
2018-08-23 14:23:03 +0800

    new file:   .idea/misc.xml 	modified:   src/bin/send.rs 	modified:   src/main.rs

--- src/bin/send.rs
+++ src/bin/send.rs
@@ -2,5 +2,7 @@ use std::net::UdpSocket;
 
 fn main(){
     let socket=UdpSocket::bind("127.0.0.1:7000").unwrap();
-    socket.send_to(&[0;1024], "127.0.0.1:6000").unwrap();
+    let mut buf=[0;1024];
+    buf[1]=1;
+    socket.send_to(&buf, "127.0.0.1:60000").unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@ use std::net::UdpSocket;
 
 
 fn main() {
-    let socket = UdpSocket::bind("0.0.0.0:6000").unwrap();
+    let socket = UdpSocket::bind("0.0.0.0:60000").unwrap();
     socket.set_nonblocking(false).unwrap();
 
-    let mut buf = vec![0_u8;65536];
-    let (num_bytes, src_addr)=socket.recv_from(&mut buf).unwrap();
-    println!("{}", num_bytes);
+    let mut buf = vec![0_u8;65536*1024];
+    let mut shift=0_usize;
+    for i in 0..128{
+        let (num_bytes, _src_addr) = socket.recv_from(&mut buf[shift..]).unwrap();
+
+        let header: &[u64] = unsafe { std::mem::transmute(&buf[num_bytes..(num_bytes+8)]) };
+
+        shift+=num_bytes;
+        println!("{}", header[0]);
+    }
 }
 
