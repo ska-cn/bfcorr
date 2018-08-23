f4500a6bea0cd51cc7249d6ba80cf07293f5919e
Gu Junhua <jhgu@nao.cas.cn>
2018-08-23 15:11:28 +0800

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let mut buf = vec![0_u8;65536*1024];
     let mut shift=0_usize;
     let mut package_size=0;
-    for _i in 0..128{
+    for _i in 0..1024{
         let (num_bytes, _src_addr) = socket.recv_from(&mut buf[shift..]).unwrap();
 
         let header: &[u64] = unsafe { std::mem::transmute(&buf[shift..(shift+8)])} ;
@@ -18,7 +18,7 @@ fn main() {
         //println!("{}", id);
         package_size=num_bytes;
     }
-    for i in 0..128{
+    for i in 0..1024{
         let shift=i*package_size;
         let header:&[u64]=unsafe{std::mem::transmute(&buf[shift..(shift+8)])};
         let id=header[0] & 0b000000000000000000000111111111111111111111111111111111111111111_u64;
