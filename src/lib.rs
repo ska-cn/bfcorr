0decbd2d87ad50f4d63ae4493bef59b5e967cf63
jhgu <jhgu@nao.cas.cn>
2018-08-25 17:20:01 +0800

    modified:   src/bin/server.rs 	modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,69 +1,86 @@
 extern crate pcap;
 
-use std::sync::RwLock;
-use std::sync::atomic::AtomicUsize;
-use std::sync::atomic::Ordering;
-use std::cell::RefCell;
+extern crate crossbeam_channel;
+use crossbeam_channel as channel;
+use pcap::Capture;
 
-pub const ID_MASK:u64=((1_u64<<42)-1);
-pub const BYTES_PER_NUMBER:usize=2;
+pub const ID_MASK: u64 = ((1_u64 << 42) - 1);
+pub const BYTES_PER_NUMBER: usize = 2;
 
+pub fn run_daq(
+    dev_name: &str,
+    port: u16,
+    nchannels: usize,
+    nchunks: usize,
+    queue_depth: usize,
+) -> channel::Receiver<(usize, Vec<i16>)> {
+    let dev = pcap::Device {
+        name: dev_name.to_string(),
+        desc: None,
+    };
 
-#[derive(Clone)]
-pub enum PacketPayload{
-    Unused(Vec<i16>),
-    Used
-}
+    let mut cap = Capture::from_device(dev)
+        .unwrap()
+        .timeout(1000000000)
+        .buffer_size(512 * 1024 * 1024)
+        .open()
+        .unwrap();
+    cap.filter(&format!("dst port {}", port)).unwrap();
+    //cap.next().unwrap();
+    let buf_size = nchannels * nchunks * 2;
 
-pub struct DataReceiver{
-    chunk_per_buffer:usize,
-    payload_bytes:usize,//in bytes
-    current_writing_buffer:AtomicUsize,
-    buffer:[RwLock<Vec<PacketPayload>>;2],
-    capture:RefCell<pcap::Capture<pcap::Active>>
-}
+    let (send, recv): (channel::Sender<(usize, Vec<i16>)>, channel::Receiver<(usize, Vec<i16>)>) =
+        channel::bounded(queue_depth);
 
-
-impl DataReceiver{
-    fn new(device_name:&str, port:usize, 
-    chunk_per_buffer:usize,nchannels:usize    
-    )->DataReceiver{
-        let payload_bytes=nchannels*BYTES_PER_NUMBER;
-        let buffer=[RwLock::new(vec![PacketPayload::Used; chunk_per_buffer]), RwLock::new(vec![PacketPayload::Used; chunk_per_buffer])];
-        let device=pcap::Device{name:device_name.to_string(), desc:None};
-        let mut capture=pcap::Capture::from_device(device).unwrap()
-                .timeout(1000000000)
-                .buffer_size(512*1024*1024)
-                .open().unwrap();
-        capture.filter(&format!("dst port {}", port));
-
-        DataReceiver{
-            chunk_per_buffer:chunk_per_buffer,    
-            payload_bytes:payload_bytes,
-            current_writing_buffer:AtomicUsize::new(0),
-            buffer:buffer,
-            capture:RefCell::new(capture)
-        }
-    }
-
-
-    fn run(&self)->!{
-        self.current_writing_buffer.store(0, Ordering::Relaxed);
-        //let w=vec![3,2];
-        loop{
-            let mut cap=self.capture.borrow_mut();
+    let recv1=recv.clone();
+    let th = std::thread::spawn(move || {
+        let mut old_id={
             let packet=cap.next().unwrap();
             let data=&packet.data[42..];
-            //println!("{}", data.len());
-            let payload:&[i16]=unsafe{std::mem::transmute(&data[8..])};
-            let header:&[u64]=unsafe{std::mem::transmute(&data[0..(0+8)])};
-            //let id=header[0] & 0b000000000000000000000111111111111111111111111111111111111111111_u64;
+            let payload=&data[8..];
+            let header:&[u64]=unsafe { std::mem::transmute(&data[0..(0 + 8)]) };
             let id=header[0] & ID_MASK;
-            let current_buffer_id=(id as usize/self.chunk_per_buffer)%2;
-            let next_buffer_id=((id as usize+1)/self.chunk_per_buffer)%2;
-            //w=4;
-            //let trunk_id=(id as usize)%NUM_TRUNKS;
-            //buf[trunk_id as usize*PAYLOAD_LEN..(trunk_id as usize*PAYLOAD_LEN+PAYLOAD_LEN)].copy_from_slice(payload);
+            id
+        };
+        let mut cnt=0;
+        let mut buf = vec![0_i16; buf_size];
+        while let Ok(packet) = cap.next() {
+            //println!("received packet! {:?}", packet);
+            //println!("{}", packet.data.len());
+            let data: &[u8] = &packet.data[42..];
+            //println!("{}", data.len());
+            let payload = &data[8..];
+            if payload.len() != nchannels * BYTES_PER_NUMBER * 2 {
+                println!("{}", payload.len());
+            }
+            assert!(payload.len() == nchannels * BYTES_PER_NUMBER * 2);
+            let header: &[u64] = unsafe { std::mem::transmute(&data[0..(0 + 8)]) };
+            //let id=header[0] & 0b000000000000000000000111111111111111111111111111111111111111111_u64;
+            let id = header[0] & ID_MASK;
+
+            if (id as usize/nchunks)>(old_id as usize/nchunks){
+                let old_buf=std::mem::replace(&mut buf, vec![0_i16; buf_size]);
+                if recv1.is_full(){
+                    recv1.recv();
+                }
+                send.send((old_id as usize/nchunks, old_buf));
+                println!("a");
+            }
+
+            let trunk_id = (id as usize) % nchunks;
+            let converted_data: &[i16] = unsafe {
+                std::slice::from_raw_parts(payload.as_ptr() as *const i16, nchannels * 2)
+            };
+            //println!("{} {} {}", payload.len(), converted_data.len(), nchannels*2);
+            buf[trunk_id as usize * nchannels * 2..((trunk_id + 1) as usize * nchannels * 2)]
+                .copy_from_slice(converted_data);
+
+            old_id=id;
+            if cnt%10000==0{
+                println!("{}", id);
+            }
+            cnt+=1;
         }
-    }
+    });
+    recv
 }
